1ca5e020937029e278ae749dc4b395d7bd36e826
Leonardo <leonardo.saes16@gmail.com>
2024-06-13 11:12:16 -0300

    adicionando um novo carrossel

--- js/script.js
+++ js/script.js
@@ -6,24 +6,6 @@ function closeMenu(){
     document.getElementById("menu-logo-fechar").style.width = "0%";
 }
 
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-});
-
 new Glide(".images",{
   type:'carousel',
   perView: 4,
@@ -41,6 +23,25 @@ new Glide(".images",{
   }
 }
 }).mount();
+
+new Glide(".images2",{
+  type:'carousel',
+  perView: 4,
+  focusAt: 'center',
+  gap: 40,
+  breakpoints: {
+    1200:{
+      perView: 3
+    },
+    800:{
+      perView: 2
+    },
+    500:{
+      perView: 1
+  }
+}
+}).mount();
+
 
 $('#numero-participantes').mask('(00) 00000-0000');
 $('#numero-patrocinador').mask('(00) 00000-0000');
